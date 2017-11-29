b4bc2e24249ed699e106887b574e7a503fbadd71
Kylie Rydzik <kylie.rydzik@gmail.com>
2017-11-29 16:29:37 -0500

    Assignment-26-play-pause-2: completed

--- scripts/album.js
+++ scripts/album.js
@@ -78,13 +78,17 @@ var setCurrentAlbum = function(album) {
 
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
-    }
+  if (element.parentElement === null) {
+    console.log("No parent found");
+  } else if (currentParent.className !== targetClass || currentParent.className === null) {
+    console.log("No parent found with that class name");
+  } else if (element) {
+    var currentParent = element.parentElement;
+    while (currentParent.className !== targetClass && currentParent.className !== null) {
+        currentParent = currentParent.parentElement;
+  }
+    return currentParent;
+  }
 };
 
 var getSongItem = function(element) {

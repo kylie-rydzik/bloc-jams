81ee1865b6e17b02f9161c86870210496f563573
Kylie Rydzik <kylie.rydzik@gmail.com>
2017-12-14 16:24:34 -0500

    Checkpoint 31 next: completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: '4:26' },
+    { title: 'Green', duration: '3:14' },
+    { title: 'Red', duration: '5:01' },
+    { title: 'Pink', duration: '3:21' },
+    { title: 'Magenta', duration: '2:15' },
+  ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21' },
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15' },
+  ]
+};
+
+var albumNoDoubt = {
+  title: 'Tragic Kingdom',
+  artist: 'No Doubt',
+  label: 'Trauma Records',
+  year: '1995',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Spiderwebs', duration: '4:28' },
+    { title: 'Different People', duration: '4:35' },
+    { title: 'Sunday Morning', duration: '4:33' },
+    { title: 'End It on This', duration: '3:46' },
+    { title: 'Just a Girl', duration: '3:29' },
+  ]
+};
